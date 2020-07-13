8064fa1813df5854cb4b78eaa1c5577696c5c5f9
MazhaevBritt <67732135+MazhaevBritt@users.noreply.github.com>
2020-07-13 21:47:04 +1000

    Выполненное ДЗ №4

--- src/geekbrains/Java/level1/Main.java
+++ src/geekbrains/Java/level1/Main.java
@@ -4,131 +4,119 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    /* Константы для массива игрового поля, его размера и условия победы*/
+    public static char [] [] gameMap;
+    public static final int SIZE = 3;
+    public static final int DOTS_TO_WIN = 3;
+    /* Константы обозначений игрового поля*/
+    public static final char DOT_EMPTY = '.'; /* Пустая ячейка игрового поля */
+    public static final char DOT_X = 'X'; /* Обозначение "крестика" */
+    public static final char DOT_O = 'O'; /* Обозначение "нолика" */
+    public static Scanner sc = new Scanner(System.in); /* инициализация ввода пользователя */
+    public static Random rand = new Random(); /* инициализация рандома */
 
     public static void main(String[] args) {
-        System.out.println ("Здравствуйте. Пожалуйста, введите ваше имя:");
-        Scanner sc = new Scanner (System.in);
-        String userName = sc.nextLine ();
-
-        /* 1. Написать программу, которая загадывает случайное число от 0 до 9.
-        Пользователю дается 3 попытки угадать это число.
-        При каждой попытке компьютер должен сообщить больше ли указанное пользователем число чем загаданное, или меньше.
-        После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).*/
-
-        get_Greeting (userName);
-
+        initGameMap();
+        printGameMap();
         while (true) {
-            randomNumberGame(userName);
-            System.out.println ();
-            System.out.println ("Хотите попробывать еще раз?");
-            int userChoise = sc.nextInt ();
-            if (userChoise == 0) {
-                System.out.println ("До свидания, " + userName + ".");
+            playerTurn();
+            printGameMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            computerTurn();
+            printGameMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
                 break;
             }
         }
-
-        /* 2* Создать массив из слов. При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
-        сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь.
-        Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.
-        Играем до тех пор, пока игрок не отгадает слово.
-        Используем только маленькие буквы*/
-
-        String [] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
-                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
-                "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-
-        get_Instructions (userName);
-
-        printArray (words);
-
-        get_Comparison_Words (words, userName);
+        System.out.println("Игра закончена");
 
     }
-
-    public static void get_Greeting (String userName) {
-        System.out.println ();
-        System.out.println (userName + ", добро подаловать в игру угадай число! Правила просты:");
-        System.out.println ("Вам будет загадано число, в промежутке от 0 до 9. Вы должны угадать его.");
-        System.out.println ("На это вам будет дано всего 3 попытки.");
-        System.out.println ("Если вам понятны правила, тогда приступим!");
-        System.out.println ();
-    }
-
-    public static void randomNumberGame (String userName) {
-
-        Random rand = new Random ();
-        int randomNumber = rand.nextInt (10);
-        //System.out.println (randomNumber);
-
-        System.out.println (userName + ", введите загаданное число:");
-        Scanner sc = new Scanner (System.in);
-
-        int i = 0;
-        for (; i < 3; i++) {
-            int userAnswer = sc.nextInt ();
-            if (userAnswer == randomNumber) {
-                System.out.println ("Поздравляю вас " + userName + "! Вы верно угадали число!");
-                break;
-            } else if (userAnswer < randomNumber && userAnswer >= 0) {
-                System.out.println (userName + ", вы ошиблись. Ваше число меньше загаданного. Поробуйте еще раз.");
-            } else if (userAnswer > randomNumber && userAnswer <= 9) {
-                System.out.println (userName + ", вы ошиблись. Ваше число больше загаданного. Попробуйте еще раз.");
-            } else {
-                System.out.println (userName + ", вас просили вводить только числа от 0 до 9. Повторите ввод.");
+    /* Инициализация игрового поля */
+    public static void initGameMap () {
+        gameMap = new char [SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++ ) {
+            for (int j = 0; j < SIZE; j++) {
+                gameMap [i] [j] = DOT_EMPTY;
             }
         }
-        if (i == 3) {
-            System.out.println ("Вы проиграли.");
-        }
     }
-
-    public static void get_Instructions (String userName) {
-        System.out.println (userName + ", сейчас вам выведеться список слов. Ваша задача угадать загаданное слово из этого списка.");
-        System.out.println ("Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.");
-        System.out.println ("Играем до тех пор, пока вы не отгадаете слово");
-        System.out.println ("И так, поехали!");
-        System.out.println ();
-    }
-
-    public static void printArray (String [] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-            System.out.println ();
-        }
+    /* Вывод игрового поля, с использованием инициализированного массива */
+    public  static void printGameMap () {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        } /* Вывод "верхних" координат игрового поля */
         System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(gameMap[i][j] + " ");
+            } /* Вывод пустых ячеек игрового поля, для каждого ряда массива */
+            System.out.println();
+        } /* Вывод "боковых" координат игрового поля */
+        System.out.println();
+
+    }
+    /* Метод, описывающий ход ИГРОКА, с учетом того, что Игрок вводит координаты больше индексов массива на 1 */
+    public static void playerTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); /* Цикл работает до тех пор, пока ИГРОК выбирает путые координаты, и которые относятся к индексам массива. */
+        gameMap[y][x] = DOT_X;
+    }
+    /* Сообственно осуществляем проверку - можно поставить "крестик" или нет */
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (gameMap[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+    /* Метод, описывающий ход компьютера */
+    public static void computerTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE); /* Выбор рандомной координаты x , из диапозона SIZE */
+            y = rand.nextInt(SIZE); /* Выбор рандомной координаты y , из диапозона SIZE */
+        } while (!isCellValid(x, y)); /* Осуществление проверки, возможно ли походить по данным координатам */
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        gameMap[y][x] = DOT_O;
+    }
+    /* Проверка условий победы */
+    public static boolean checkWin (char symb) {
+        for (int i = 0; i < 3; i++)
+            if ((gameMap[i][0] == symb && gameMap[i][1] == symb &&
+                    gameMap[i][2] == symb) ||
+                    (gameMap[0][i] == symb && gameMap[1][i] == symb &&
+                            gameMap[2][i] == symb))
+                return true;
+        if ((gameMap[0][0] == symb && gameMap[1][1] == symb &&
+                gameMap[2][2] == symb) ||
+                (gameMap[2][0] == symb && gameMap[1][1] == symb &&
+                        gameMap[0][2] == symb))
+            return true;
+        return false;
     }
 
-    public  static void get_Comparison_Words (String [] words, String userName) {
-
-        Random rand = new Random ();
-        int randomWord = rand.nextInt (25);
-
-        String hiddenWord = words [randomWord];
-        /*System.out.println (hiddenWord);*/
-        String str1 = hiddenWord;
-
-        int check = 0;
-        while (check != str1.length()) {
-
-            Scanner word = new Scanner (System.in);
-            System.out.println ("Введите слово:");
-            String userWord = word.nextLine();
-            String str2 = userWord;
-
-            for (int i = 0; i < str1.length (); i++) {
-                if (str1.charAt(i) == str2.charAt(i)) {
-                    System.out.print(str1.charAt(i));
-                    check++;
-                } else {
-                    System.out.print ("###############");
-                    System.out.println();
-                    System.out.println ("Попробуйте еще раз.");
-                    break;
-                }
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (gameMap[i][j] == DOT_EMPTY) return false;
             }
         }
-        System.out.println();
-        System.out.println ("Поздравляю! Вы отгадали слово!");
+        return true;
     }
+
 }
